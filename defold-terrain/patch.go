1e82f55500b4be064ff37d554c2abf9e8fa660f4
Mathias Westerdahl <mwesterdahl76@gmail.com>
2025-12-21 17:31:58 +0100

    Removed old patch collection

---
+++ defold-terrain/patch.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "mesh"
+  type: "mesh"
+  data: "material: \"/defold-terrain/materials/terrain.material\"\n"
+  "vertices: \"/defold-terrain/meshes/empty.buffer\"\n"
+  "textures: \"/assets/images/green.png\"\n"
+  "primitive_type: PRIMITIVE_TRIANGLES\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
