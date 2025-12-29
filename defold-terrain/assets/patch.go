3343e350a3d32b077d00c5c7a614160d24a343d5
Mathias Westerdahl <mwesterdahl76@gmail.com>
2025-12-29 12:15:35 +0100

    moved assets into assets folder

---
+++ defold-terrain/assets/patch.go
@@ -0,0 +1,8 @@
+embedded_components {
+  id: "mesh"
+  type: "mesh"
+  data: "material: \"/defold-terrain/assets/materials/terrain.material\"\n"
+  "vertices: \"/defold-terrain/assets/empty.buffer\"\n"
+  "textures: \"/assets/images/green.png\"\n"
+  ""
+}
